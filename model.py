c58e3bf7d6af62e86c8206bfde081c60da362821
bala1802 <bala1802@live.com>
2023-09-11 15:48:22 +0530

    added skeleton to the Encoder Layer

--- model.py
+++ model.py
@@ -138,3 +138,11 @@ class EncoderBlock(nn.Module):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
         x = self.residual_connections[1](x, self.feed_forward_block)
         return(x)
+
+#Encoder Layer
+class Encoder(nn.Module):
+    def __init__(self, layers:nn.ModuleList):
+        pass
+
+    def forward(self, x, mask):
+        pass
