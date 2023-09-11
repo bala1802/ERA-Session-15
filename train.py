e6fe41c27bceebefc1933ff2bc4b99b32025a971
bala1802 <bala1802@live.com>
2023-09-11 20:20:03 +0530

    training code is in progress

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import torchtext.datasets as datasets
 
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader,  random_split
+from torch.utils.data import Dataset, DataLoader, random_split
 from torch.optim.lr_scheduler import LambdaLR
 
 import warnings
@@ -23,8 +23,56 @@ from tokenizers.pre_tokenizers import Whitespace
 import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
 
+#TODO
+def get_ds(config):
+    ds_raw = load_dataset
+    return "train_loader", "val_loader", "tokenizer_src", "tokenizer_tgt"
+
+'''
+The transformer block is built and retreived from the model script
+'''
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer_block(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], d_model=config['d_model'])
+    return model
+
 def train_model(config):
-    pass
+    device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+    print("The device used is : ", device)
+    Path(config['model_folder']).mkdir(parents=True,exist_ok=True)
+    
+    train_loader, val_loader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    writer = SummaryWriter(config['experiment_name'])
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], eps=1e-6)
+
+    initial_step = 0
+    global_step = 0
+
+    if config['preload']:
+        model_filename = get_weights_file_path(config, config['preload'])
+        print(f'Preloading model {model_filename}')
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+        print("Preloaded")
+    
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+        batch_iterator = tqdm(train_loader, desc=f"Processing Epoch {epoch:02d}")
+        
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input'].to(device)
+            
+        pass
+
+
+
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
