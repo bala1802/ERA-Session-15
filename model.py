bdeeb682cfa4dfeb010a5bfffd157ffcaaf9fad5
bala1802 <bala1802@live.com>
2023-09-11 14:16:36 +0530

    added residual connection

--- model.py
+++ model.py
@@ -65,3 +65,13 @@ class PositionalEncoding(nn.Module):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         x = self.dropout(x)
         return x
+    
+#Residual Connection Layer
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout:float):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+    
+    def forward(self, x, sublayer):
+        return (x + self.dropout(sublayer(self.norm(x))))
