54f720253f56fb748cedae0c383f25d88a303c7c
bala1802 <bala1802@live.com>
2023-09-11 15:44:19 +0530

    added EncoderBlock

--- model.py
+++ model.py
@@ -125,3 +125,16 @@ class MultiHeadAttentionBlock(nn.Module):
         #(batch, seq_Length, d_model) -> (batch, seq_len, d_model)
 
         return (self.w_o(x))
+    
+#Encoder block
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block:MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout:float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout for _ in range(2))])
+    
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return(x)
