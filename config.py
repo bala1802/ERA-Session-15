61ce7bceb6442a89a76cfb5d4a0e380cb6c42346
bala1802 <bala1802@live.com>
2023-09-11 17:02:58 +0530

    feature to get the model weights for the specified epoch

--- config.py
+++ config.py
@@ -15,3 +15,9 @@ def get_config():
         'tokenizer_file': 'tokenizer_{0}.json',
         'experiment_name': 'runs/tmodel'
     }
+
+def get_weights_file_path(config, epoch:str):
+    model_folder = config["model_folder"]
+    model_basename = config["model_basename"]
+    model_filename = f'{model_basename}{epoch}.pt'
+    return (str(Path('.') / model_folder / model_filename))
