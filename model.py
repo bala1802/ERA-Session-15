b303263332963bc1cf29ed89b9ba6fcdedae6201
bala1802 <bala1802@live.com>
2023-09-11 11:47:51 +0530

    added layer normalization and feedforward neural network block

---
+++ model.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import math
+
+#Normalization Layer
+class LayerNormalization(nn.Module):
+
+    def __init__(self, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.ones(1))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return (self.alpha * (x-mean)/ (std + self.eps) + self.bias)
+    
+#FeedForward Neural Network
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.relu(x)
+        x - self.dropout(x)
+        x = self.linear2(x)
+        return self.linear2(self.dropout(torch.relu((self.linear1(x)))))
+        
