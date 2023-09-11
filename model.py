5635f73b4694120c45cd31421e62240f299d18cd
bala1802 <bala1802@live.com>
2023-09-11 11:58:37 +0530

    added embedding layer

--- model.py
+++ model.py
@@ -31,4 +31,14 @@ class FeedForwardBlock(nn.Module):
         x - self.dropout(x)
         x = self.linear2(x)
         return self.linear2(self.dropout(torch.relu((self.linear1(x)))))
-        
+    
+#Embedding Layer
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model:int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return (self.embedding(x)) * math.sqrt(self.d_model)        
