66956bbdcfad0cf9bd9e2457f92284141e617c27
bala1802 <bala1802@live.com>
2023-09-11 16:05:24 +0530

    initialized the Transformer Block

--- model.py
+++ model.py
@@ -195,8 +195,24 @@ class ProjectionLayer(nn.Module):
 
 #Transformers Block
 class Transformer(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, 
+                 encoder: Encoder, 
+                 decoder: Decoder, 
+                 src_embed: InputEmbeddings, 
+                 tgt_embed: InputEmbeddings,
+                 src_pos: PositionalEncoding,
+                 tgt_pos: PositionalEncoding,
+                 projection_layer: ProjectionLayer):
+        
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
 
     def encode(self):
         pass
