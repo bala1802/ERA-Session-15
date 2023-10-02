968ace91c51d1cde15766ddfe23aedf8a9fdfcd3
bala1802 <bala1802@live.com>
2023-10-02 11:43:28 +0530

    added the module to create Tokenizers for the given dataset

--- datamodule.py
+++ datamodule.py
@@ -19,13 +19,24 @@ OpusBookDataModule - Class to construct the dataset.
 class OpusBookDataModule(pl.LightningDataModule):
 
     def __init__(self, config):
-        pass
+        super().__init__()
+        self.config = config
 
-    def get_all_sentences(self, ds, config):
-        pass
+    def get_all_sentences(self, ds, lang):
+        for item in ds:
+            yield item["translation"][lang]
 
     def get_or_build_tokenizer(self, ds, lang):
-        pass
+        tokenizer_path = Path(self.config["tokenizer_file"].format(lang))
+        if Path.exists(tokenizer_path):
+            return Tokenizer.from_file(str(tokenizer_path))
+        else:
+            tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+            tokenizer.pre_tokenizer = Whitespace()
+            trainer = WordLevelTrainer(special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+            tokenizer.train_from_iterator(self.get_all_sentences(ds=ds, lang=lang), trainer=trainer)
+            tokenizer.save(str(tokenizer_path))
+            return tokenizer
 
     def setup(self, stage):
         pass
